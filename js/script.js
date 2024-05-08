7f978575b499dc265216be15b554619816ec63f9
rushitakanani <rushikanani014@gmail.com>
2024-05-08 09:19:37 +0530

    final commit

---
+++ js/script.js
@@ -0,0 +1,43 @@
+$('#slider-2').owlCarousel({
+  loop:true,
+  margin:10,
+  nav:false,
+  dots:false,
+  margin:30,
+  responsive:{
+      0:{
+          items:1
+      },
+      600:{
+          items:3
+      },
+      1000:{
+          items:5
+      }
+  }
+})
+$('#slider-1').owlCarousel({
+  loop:true,
+  margin:10,
+  nav:false,
+  dots:false,
+  margin:25,
+  responsive:{
+      0:{
+          items:1
+      },
+      600:{
+          items:2
+      },
+      1000:{
+          items:3
+      }
+  }
+})
+$('#slider-3').owlCarousel({
+    loop:true,
+    nav:true,
+    dots:false,
+    items:1,
+  })
+  
